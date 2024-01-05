9950aa2bd2a939a80996bff1c643d835b807df2e
daniherreraa <herreraacevedodl@gmail.com>
2024-01-04 23:35:28 -0500

    Update tech versions and mobile style for dashboard/sidenav component

--- src/app/ui/dashboard/sidenav.jsx
+++ src/app/ui/dashboard/sidenav.jsx
@@ -1,10 +1,12 @@
-import Link from 'next/link';
-import DarkTheme from './darktheme';
+import Link from "next/link";
+import DarkTheme from "./darktheme";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-6 md:px-2 bg-on-background text-primary-dark">
-      <h1 className='text-2xl text-on-primary' style={{fontFamily: 'clash'}}>Luma</h1>
+    <div className="flex h-full flex-row justify-between md:flex-col px-3 py-6 md:px-2 bg-on-background text-primary-dark">
+      <h1 className="text-2xl text-on-primary" style={{ fontFamily: "clash" }}>
+        Luma
+      </h1>
       <DarkTheme />
     </div>
   );
