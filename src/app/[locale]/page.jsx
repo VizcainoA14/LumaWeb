c33e8b94b2bf0954d5e4cfc73efc8eb77969bca5
DonadoM <donadom@utb.edu.co>
2024-01-16 17:40:46 -0500

    Primer Día

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,18 +1,41 @@
-import {useTranslations} from 'next-intl';
- 
+import { useTranslations } from "next-intl";
+
 export default function Index() {
-  const t = useTranslations('Landing');
-  return(
-    <div className='w-screen h-fit flex'>
+  const t = useTranslations("Landing");
+  return (
+    <div className="w-screen h-fit flex">
       <div id="header" className="w-full h-[100svh] md:flex-row">
-        <div id='titlesContainer' className="w-full h-3/5 relative flex">
-          <h1>{t('title')}</h1>
-          <div className='absolute w-28 h-28 bg-primary top-0 right-0'>g</div>
+        <div
+          id="Hero"
+          className="w-full h-3/5 relative flex items-center p-14 "
+        >
+          <div
+            className="w-2/3 h-full flex flex-col"
+            style={{ fontFamily: "Clash" }}
+          >
+            <h1 className="self-center text-5xl">{t("title")}</h1>
+            <button className="bg-on-background text-background mt-4">
+              Get started
+            </button>
+          </div>
+          <div
+            className="w-1/3 h-full flex flex-col justify-center"
+            style={{ fontFamily: "Archivo" }}
+          >
+            <ul className="self-center">
+              <li>Easy to use</li>
+              <li>Powerfull</li>
+              <li>Open to Everybody</li>
+            </ul>
+          </div>
         </div>
-        
-        <div className="w-full h-2/5 bg-secondary"></div>
+
+        <div className="w-full h-2/5 bg-secondary grid grid-cols-3 gap-4">
+          <div className="bg-gray-300 p-4 bg-primary-dark">Columna 1</div>
+          <div className="bg-gray-300 p-4 bg-primary-container">Columna 2</div>
+          <div className="bg-gray-300 p-4">Columna 3</div>
+        </div>
       </div>
     </div>
-    
-  )
+  );
 }
