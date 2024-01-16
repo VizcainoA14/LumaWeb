c0294327d197dfac087a0861452d959255a10fdf
daniherreraa <herreraacevedodl@gmail.com>
2024-01-16 15:08:55 -0500

    Update landing page title and add a new element

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,13 +1,15 @@
 import {useTranslations} from 'next-intl';
  
 export default function Index() {
-  const t = useTranslations('Index');
+  const t = useTranslations('Landing');
   return(
     <div className='w-screen h-fit flex'>
       <div id="header" className="w-full h-[100svh] md:flex-row">
-        <div id='titlesContainer' className="w-full h-3/5">
+        <div id='titlesContainer' className="w-full h-3/5 relative flex">
           <h1>{t('title')}</h1>
+          <div className='absolute w-28 h-28 bg-primary top-0 right-0'>g</div>
         </div>
+        
         <div className="w-full h-2/5 bg-secondary"></div>
       </div>
     </div>
