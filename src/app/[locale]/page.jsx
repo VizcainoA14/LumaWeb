10370ace9c8efb8d663e3b34bcb1ebb4a510fd6e
DonadoM <donadom@utb.edu.co>
2024-01-17 15:13:33 -0500

    Segundo día

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -4,25 +4,25 @@ export default function Index() {
   const t = useTranslations("Landing");
   return (
     <div className="w-screen h-fit flex">
-      <div id="header" className="w-full h-[100svh] md:flex-row">
+      <div id="header" className="w-full h-[100svh] flex-col md:flex-row">
         <div
           id="Hero"
-          className="w-full h-3/5 relative flex items-center p-14 "
+          className="w-full md:h-3/5 relative flex md:items-center p-2 md:p-14  flex-col md:flex-row"
         >
           <div
-            className="w-2/3 h-full flex flex-col"
+            className="md:w-2/3 md:h-full flex flex-col "
             style={{ fontFamily: "Clash" }}
           >
-            <h1 className="self-center text-5xl">{t("title")}</h1>
+            <h1 className=" self-center text-1xl md:text-5xl p-1">{t("title")}</h1>
             <button className="bg-on-background text-background mt-4">
               Get started
             </button>
           </div>
           <div
-            className="w-1/3 h-full flex flex-col justify-center"
+            className="md:w-1/3 md:h-full flex items-start  p-3 md:p-1 text-1xl md:items-start "
             style={{ fontFamily: "Archivo" }}
           >
-            <ul className="self-center">
+            <ul className="self-center text-on-background/60">
               <li>Easy to use</li>
               <li>Powerfull</li>
               <li>Open to Everybody</li>
