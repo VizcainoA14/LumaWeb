b79ce29f38fdbda4b3a779c55974bb71895c086d
daniherreraa <herreraacevedodl@gmail.com>
2024-01-24 15:05:09 -0500

    Refactor SideNav component

--- src/app/ui/dashboard/sidenav.jsx
+++ src/app/ui/dashboard/sidenav.jsx
@@ -3,13 +3,18 @@ import NavLinks from "@/app/ui/dashboard/nav-links";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-row justify-between lg:flex-col px-3 py-6 md:px-2 bg-on-background" style={{ fontFamily: "clash" }}>
-      <div className="text-2xl text-on-primary">
-        Luma
+    <div
+      className="flex h-full flex-row justify-between lg:flex-col px-3 py-6 md:px-2 bg-on-background"
+      style={{ fontFamily: "clash" }}
+    >
+      <div className="">
+        <h2 className="text-2xl text-on-background-dark">Luma</h2>
+        <div className="w-40 md:w-full text-white flex flex-row md:flex-col">
+          <h6 className="text-outline-dark text-sm my-2">Mode</h6>
+          <NavLinks />
+        </div>
       </div>
-      <div className="w-40 md:w-full text-white flex flex-row md:flex-col">
-        <NavLinks />
-      </div>
+
       <DarkTheme />
     </div>
   );
