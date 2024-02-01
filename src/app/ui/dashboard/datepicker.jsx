33b9c6f5f591686f7df848ed02414ba1dd3c8021
daniherreraa <herreraacevedodl@gmail.com>
2024-02-01 17:31:01 -0500

    Improving datepickers Vol.1

--- src/app/ui/dashboard/datepicker.jsx
+++ src/app/ui/dashboard/datepicker.jsx
@@ -39,6 +39,9 @@ export function DatePicker({onDateChange}) {
       <PopoverContent className="w-auto p-0 bg-tertiary-container text-on-tertiary-container dark:bg-tertiary-container-dark dark:text-on-tertiary-container-dark mt-1 mx-4 md:mx-10">
         <Calendar
           mode="single"
+          captionLayout="dropdown-buttons"
+          fromYear={2011}
+          toYear={2023}
           selected={date}
           onSelect={handleDateChange}
           initialFocus
