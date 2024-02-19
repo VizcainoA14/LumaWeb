34d53e80b083dada614540c56ff3cc754faa2488
daniherreraa <herreraacevedodl@gmail.com>
2024-02-19 13:48:41 -0500

    Añadida animación de entrada para el título del héroe en la página principal

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -5,13 +5,18 @@ import Image from "next/image";
 import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 export default function Index() {
+  const heroTitle = useRef();
   const t = useTranslations("Landing");
 
   // Using Gsap
   useGSAP(() => {
-
+    gsap.from(".tLetter", {
+      y:300,
+      stagger: 0.1,
+    }, { scope: heroTitle})
   });
 
   return (
@@ -28,13 +33,16 @@ export default function Index() {
         />
         <div
           id="heroTitles"
-          className="absolute text-[13.5vw] font-bold text-on-background-dark top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          className="absolute text-[14vw] font-bold text-on-background-dark top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
           style={{ fontFamily: "Clash" }}
         >
           <div className="titleWrapper h-fit overflow-hidden">
-            <h1 ref={heroTitle} id="heroTitle" className="leading-none">
-              LUMA
-            </h1>
+            <div ref={heroTitle} id="heroTitle" className="flex leading-none">
+              <p className="tLetter">L</p>
+              <p className="tLetter">U</p>
+              <p className="tLetter">M</p>
+              <p className="tLetter">A</p>
+            </div>
           </div>
         </div>
       </div>
