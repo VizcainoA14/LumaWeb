d997929c5b4c9c14e44e540d600cd5556d08dfde
Adrian Vizcaino <avizcaino@utb.edu.co>
2024-04-29 21:39:25 -0500

    Using SQLite for data

--- src/app/api/get-data/route.js
+++ src/app/api/get-data/route.js
@@ -1,8 +1,14 @@
-import { db } from "@vercel/postgres";
-import { NextResponse } from "next/server";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { NextResponse } from 'next/server';
 
 export async function GET(request) {
-  const client = await db.connect();
+  // Abre la base de datos SQLite
+  const db = await open({
+    filename: 'src//app//api//DATA.sqlite',
+    driver: sqlite3.Database
+  });
+
   const { searchParams } = new URL(request.url);
   const date = searchParams.get("date");
   let year = date.substring(0, 4);
@@ -15,51 +21,175 @@ export async function GET(request) {
   let datahmimag;
 
   try {
-    data171 = await client.sql`
-    SELECT * 
-    FROM eit171
-    WHERE SUBSTRING(Date, 1, 4) = ${year} AND SUBSTRING(Date, 6, 2) = ${month}
-    `;
+    // Realiza las consultas SQL utilizando SQLite
+    data171 = await db.all(`
+      SELECT * 
+      FROM eit171
+      WHERE SUBSTR(Date, 1, 4) = ? AND SUBSTR(Date, 6, 2) = ?
+    `, [year, month]);
 
-    data195 = await client.sql`
-    SELECT * 
-    FROM eit195
-    WHERE SUBSTRING(Date, 1, 4) = ${year} AND SUBSTRING(Date, 6, 2) = ${month}
-    `;
+    data195 = await db.all(`
+      SELECT * 
+      FROM eit195
+      WHERE SUBSTR(Date, 1, 4) = ? AND SUBSTR(Date, 6, 2) = ?
+    `, [year, month]);
 
-    data284 = await client.sql`
-    SELECT * 
-    FROM eit284
-    WHERE SUBSTRING(Date, 1, 4) = ${year} AND SUBSTRING(Date, 6, 2) = ${month}
-    `;
+    data284 = await db.all(`
+      SELECT * 
+      FROM eit284
+      WHERE SUBSTR(Date, 1, 4) = ? AND SUBSTR(Date, 6, 2) = ?
+    `, [year, month]);
 
-    data304 = await client.sql`
-    SELECT * 
-    FROM eit304
-    WHERE SUBSTRING(Date, 1, 4) = ${year} AND SUBSTRING(Date, 6, 2) = ${month}
-    `;
+    data304 = await db.all(`
+      SELECT * 
+      FROM eit304
+      WHERE SUBSTR(Date, 1, 4) = ? AND SUBSTR(Date, 6, 2) = ?
+    `, [year, month]);
 
-    datahmiigr = await client.sql`
-    SELECT * 
-    FROM hmiigr
-    WHERE SUBSTRING(Date, 1, 4) = ${year} AND SUBSTRING(Date, 6, 2) = ${month}
-    `;
+    datahmiigr = await db.all(`
+      SELECT * 
+      FROM hmiigr
+      WHERE SUBSTR(Date, 1, 4) = ? AND SUBSTR(Date, 6, 2) = ?
+    `, [year, month]);
 
-    datahmimag = await client.sql`
-    SELECT * 
-    FROM hmimag
-    WHERE SUBSTRING(Date, 1, 4) = ${year} AND SUBSTRING(Date, 6, 2) = ${month}
-    `;
+    datahmimag = await db.all(`
+      SELECT * 
+      FROM hmimag
+      WHERE SUBSTR(Date, 1, 4) = ? AND SUBSTR(Date, 6, 2) = ?
+    `, [year, month]);
   } catch (error) {
+    // Manejar errores
     return NextResponse.json({ error });
+  } finally {
+    // Cierra la conexión con la base de datos al finalizar
+    await db.close();
   }
 
+  // Devuelve los resultados
   return NextResponse.json({
-    data171,
-    data195,
-    data284,
-    data304,
-    datahmiigr,
-    datahmimag
+    data171: {
+      rows: data171.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    data195: {
+      rows: data195.map(item => ({
+      date: item.date,
+      url: item.url,
+      entropy: item.entropy,
+      mean_intensity: item.mean_intensity,
+      standard_deviation: item.standard_deviation,
+      skewness: item.skewness,
+      kurtosis: item.kurtosis,
+      relative_smoothness: item.relative_smoothness,
+      uniformity: item.uniformity,
+      fractal_dimension: item.fractal_dimension,
+      taruma_contrast: item.taruma_contrast,
+      taruma_directionality: item.taruma_directionality,
+      taruma_coarseness: item.taruma_coarseness,
+      taruma_linelikeness: item.taruma_linelikeness,
+      taruma_regularity: item.taruma_regularity,
+      taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    data284: {
+      rows: data284.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    data304: {
+      rows: data304.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    datahmiigr: {
+      rows: datahmiigr.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    datahmimag: {
+      rows: datahmimag.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
   });
 }
+
+
+
+
--- src/app/api/get-range/route.js
+++ src/app/api/get-range/route.js
@@ -1,11 +1,17 @@
-import { db } from "@vercel/postgres";
-import { NextResponse} from "next/server";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  const client = await db.connect();
   const { searchParams } = new URL(request.url)
   const startDate = searchParams.get('startDate')
   const endDate = searchParams.get('endDate')
+
+  const db = await open({
+    filename: 'src//app//api//DATA.sqlite',
+    driver: sqlite3.Database
+  });
+
   let data171;
   let data195;
   let data284;
@@ -14,45 +20,135 @@ export async function GET(request) {
   let datahmimag;
 
   try {
-    data171 = await client.sql`
-        SELECT * 
-        FROM eit171
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    data195 = await client.sql`
-        SELECT * 
-        FROM eit195
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    data284 = await client.sql`
-        SELECT * 
-        FROM eit284
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    data304 = await client.sql`
-        SELECT * 
-        FROM eit304
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    datahmiigr = await client.sql`
-        SELECT * 
-        FROM hmiigr
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    datahmimag = await client.sql`
-        SELECT * 
-        FROM hmimag
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
+    data171 = await db.all(`SELECT * FROM eit171 WHERE Date BETWEEN ? AND ?`, [startDate, endDate]);
+    data195 = await db.all(`SELECT * FROM eit195 WHERE Date BETWEEN ? AND ?`, [startDate, endDate]);
+    data284 = await db.all(`SELECT * FROM eit284 WHERE Date BETWEEN ? AND ?`, [startDate, endDate]);
+    data304 = await db.all(`SELECT * FROM eit304 WHERE Date BETWEEN ? AND ?`, [startDate, endDate]);
+    datahmiigr = await db.all(`SELECT * FROM hmiigr WHERE Date BETWEEN ? AND ?`, [startDate, endDate]);
+    datahmimag = await db.all(`SELECT * FROM hmimag WHERE Date BETWEEN ? AND ?`, [startDate, endDate]);
   } catch (error) {
-    return NextResponse.json({error});
+    return NextResponse.json({ error });
   }
-
-  return NextResponse.json({data171, data195, data284, data304, datahmiigr, datahmimag})
+  return NextResponse.json({
+    data171: {
+      rows: data171.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    data195: {
+      rows: data195.map(item => ({
+      date: item.date,
+      url: item.url,
+      entropy: item.entropy,
+      mean_intensity: item.mean_intensity,
+      standard_deviation: item.standard_deviation,
+      skewness: item.skewness,
+      kurtosis: item.kurtosis,
+      relative_smoothness: item.relative_smoothness,
+      uniformity: item.uniformity,
+      fractal_dimension: item.fractal_dimension,
+      taruma_contrast: item.taruma_contrast,
+      taruma_directionality: item.taruma_directionality,
+      taruma_coarseness: item.taruma_coarseness,
+      taruma_linelikeness: item.taruma_linelikeness,
+      taruma_regularity: item.taruma_regularity,
+      taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    data284: {
+      rows: data284.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    data304: {
+      rows: data304.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    datahmiigr: {
+      rows: datahmiigr.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+    datahmimag: {
+      rows: datahmimag.map(item => ({
+        date: item.date,
+        url: item.url,
+        entropy: item.entropy,
+        mean_intensity: item.mean_intensity,
+        standard_deviation: item.standard_deviation,
+        skewness: item.skewness,
+        kurtosis: item.kurtosis,
+        relative_smoothness: item.relative_smoothness,
+        uniformity: item.uniformity,
+        fractal_dimension: item.fractal_dimension,
+        taruma_contrast: item.taruma_contrast,
+        taruma_directionality: item.taruma_directionality,
+        taruma_coarseness: item.taruma_coarseness,
+        taruma_linelikeness: item.taruma_linelikeness,
+        taruma_regularity: item.taruma_regularity,
+        taruma_roughness: item.taruma_roughness,
+      }))
+    },
+  });
 }
