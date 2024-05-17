86ba81faa1de4a57005ffcf41761534ad16c2f8d
Adrian Vizcaino <avizcaino@utb.edu.co>
2024-05-17 13:23:10 -0500

    Refactor API to Sqlite

--- src/app/api/get-range/route.js
+++ src/app/api/get-range/route.js
@@ -1,11 +1,13 @@
-import { db } from "@vercel/postgres";
-import { NextResponse} from "next/server";
+import { NextResponse } from "next/server";
+import data from "../get-data/pr.js"
+
+const prisma = data;
 
 export async function GET(request) {
-  const client = await db.connect();
   const { searchParams } = new URL(request.url)
   const startDate = searchParams.get('startDate')
   const endDate = searchParams.get('endDate')
+
   let data171;
   let data195;
   let data284;
@@ -14,45 +16,33 @@ export async function GET(request) {
   let datahmimag;
 
   try {
-    data171 = await client.sql`
-        SELECT * 
-        FROM eit171
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    data195 = await client.sql`
-        SELECT * 
-        FROM eit195
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    data284 = await client.sql`
-        SELECT * 
-        FROM eit284
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    data304 = await client.sql`
-        SELECT * 
-        FROM eit304
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    datahmiigr = await client.sql`
-        SELECT * 
-        FROM hmiigr
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
-    datahmimag = await client.sql`
-        SELECT * 
-        FROM hmimag
-        WHERE DATE(Date) BETWEEN ${startDate} AND ${endDate}
-    `;
-
+    data171 = await prisma.$queryRaw`SELECT * FROM eit171 WHERE Date BETWEEN ${startDate} AND ${endDate}`;
+    data195 = await prisma.$queryRaw`SELECT * FROM eit195 WHERE Date BETWEEN ${startDate} AND ${endDate}`;
+    data284 = await prisma.$queryRaw`SELECT * FROM eit284 WHERE Date BETWEEN ${startDate} AND ${endDate}`;
+    data304 = await prisma.$queryRaw`SELECT * FROM eit304 WHERE Date BETWEEN ${startDate} AND ${endDate}`;
+    datahmiigr = await prisma.$queryRaw`SELECT * FROM hmiigr WHERE Date BETWEEN ${startDate} AND ${endDate}`;
+    datahmimag = await prisma.$queryRaw`SELECT * FROM hmimag WHERE Date BETWEEN ${startDate} AND ${endDate}`;
   } catch (error) {
-    return NextResponse.json({error});
+    return NextResponse.json({ error });
   }
-
-  return NextResponse.json({data171, data195, data284, data304, datahmiigr, datahmimag})
+  return NextResponse.json({
+    data171: {
+      rows: data171,
+    },
+    data195: {
+      rows: data195,
+    },
+    data284: {
+      rows: data284,
+    },
+    data304: {
+      rows: data304,
+    },
+    datahmiigr: {
+      rows: datahmiigr,
+    },
+    datahmimag: {
+      rows: datahmimag,
+    },
+  });
 }
