dd6462801c6b45fd2f72dab0e326b801b1d933e0
daniherreraa <herreraacevedodl@gmail.com>
2024-06-08 17:51:00 -0500

    feat: Update RangeChart component to use LineChart instead of AreaChart

--- src/app/ui/dashboard/Charts/RangeChart.jsx
+++ src/app/ui/dashboard/Charts/RangeChart.jsx
@@ -1,5 +1,5 @@
 import moment from "moment";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 export const RangeChart = ({ rawData, selectedTable, parameter }) => {
   let dataPoints = [];
@@ -45,25 +45,31 @@ export const RangeChart = ({ rawData, selectedTable, parameter }) => {
   return (
     <div id="RangeChartContainer" className="w-full h-[50vh]">
       <ResponsiveContainer width="100%" height="100%">
-                <AreaChart
-                width={500}
-                height={300}
-                data={fixedData}
-                margin={{
-                    top: 20,
-                    right: 50,
-                    left: 20,
-                    bottom: 5,
-                }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Area connectNulls type="monotone" dataKey={parameter} stroke={graphColor} fill={graphColor} />
-                </AreaChart>
-            </ResponsiveContainer>
+        <LineChart
+          width={500}
+          height={300}
+          data={fixedData}
+          margin={{
+            top: 20,
+            right: 50,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey={parameter}
+            stroke={graphColor}
+            strokeWidth={2}
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
