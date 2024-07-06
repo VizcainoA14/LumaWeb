6052e68c71248570b5c878503e0bb60a145995f4
daniherreraa <herreraacevedodl@gmail.com>
2024-07-05 19:15:21 -0500

    Deleting broken packages in package.json.
    - ShaderGradient: Time to look for another alternative 'til fix
    - ThreeJs

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -9,11 +9,6 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
-import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
-import * as reactSpring from "@react-spring/three";
-import * as drei from "@react-three/drei";
-import * as fiber from "@react-three/fiber";
-import { LandingNav } from "../../components/landing/landingnav";
 import { LoadingLanding } from "@/components/landing/loadinglanding";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -54,7 +49,6 @@ export default function Index() {
 
   return (
     <main className="w-[100svw] h-fit flex flex-col bg-black">
-      <LandingNav />
       <header className="relative w-full h-[100svh] flex">
         {/* Titles */}
         <div
@@ -82,16 +76,6 @@ export default function Index() {
             </button>
           </Link>
         </div>
-        {/* Gradient component */}
-        <ShaderGradientCanvas
-          importedFiber={{ ...fiber, ...drei, ...reactSpring }}
-          className="w-full h-full absolute top-0 left-0 z-0"
-        >
-          <ShaderGradient
-            control="query"
-            urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%23c8c2ea&color2=%23166683&color3=%23d0e6f3&destination=onCanvas&embedMode=off&envPreset=city&fov=30&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&type=sphere&uAmplitude=3.2&uDensity=0.1&uFrequency=5.5&uSpeed=0.4&uStrength=0.1&uTime=0&wireframe=false&zoomOut=false"
-          />
-        </ShaderGradientCanvas>
       </header>
     </main>
   );
